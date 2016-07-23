7a326f446b29feb257614fc366a338b323d7f59d
11012404 <11012404@students.southwales.ac.uk>
2016-07-24 04:12:11 +0600

    UPloaed

--- app/src/main/java/com/fahim/lokman/butoothchatapp/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/fahim/lokman/butoothchatapp/bluetoothchat/MainActivity.java
@@ -29,12 +29,12 @@ public class MainActivity extends SampleActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            BluetoothChatFragment fragment = new BluetoothChatFragment();
-            transaction.replace(R.id.sample_content_fragment, fragment);
-            transaction.commit();
-        }
+//        if (savedInstanceState == null) {
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            BluetoothChatFragment fragment = new BluetoothChatFragment();
+//            transaction.replace(R.id.sample_content_fragment, fragment);
+//            transaction.commit();
+//        }
     }
 
     @Override
