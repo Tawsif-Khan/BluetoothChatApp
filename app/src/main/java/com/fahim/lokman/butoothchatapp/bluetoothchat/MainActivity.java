a2de98ec383dce33325eda250799cc0e7387cfdc
11012404 <11012404@students.southwales.ac.uk>
2016-07-27 04:37:11 +0600

    UPloaed

--- app/src/main/java/com/fahim/lokman/butoothchatapp/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/fahim/lokman/butoothchatapp/bluetoothchat/MainActivity.java
@@ -4,7 +4,6 @@
 package com.fahim.lokman.butoothchatapp.bluetoothchat;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ViewAnimator;
