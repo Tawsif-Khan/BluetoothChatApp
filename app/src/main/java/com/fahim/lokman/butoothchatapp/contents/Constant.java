768c7a34d3cc69f11de727abeb75d52132c4f193
11012404 <11012404@students.southwales.ac.uk>
2016-09-01 02:16:25 +0600

    Create Constant.java

---
+++ app/src/main/java/com/fahim/lokman/butoothchatapp/contents/Constant.java
@@ -0,0 +1,18 @@
+package com.fahim.lokman.bluetoothchatapp.contents;
+
+import android.widget.Toast;
+
+/**
+ * Created by tawsifkhan on 7/29/16.
+ */
+public class Constant {
+
+    public static final String TEXTS = "TEXTS";
+    public static final String PHOTO = "PHOTO";
+    public static final String VOICE = "VOICE";
+    public static final String FILE = "FILE";
+
+
+
+
+}
