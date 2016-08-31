f26662a54790a02d15d6d8347d0677e9fef39896
11012404 <11012404@students.southwales.ac.uk>
2016-08-31 21:16:55 +0100

    Add files via upload

---
+++ app/src/main/java/com/fahim/lokman/butoothchatapp/contents/MessageContents.java
@@ -0,0 +1,16 @@
+package com.fahim.lokman.bluetoothchatapp.contents;
+
+/**
+ * Created by tawsifkhan on 7/29/16.
+ */
+public class MessageContents {
+
+    public int id;
+    public String message;
+    public String time;
+    public String type;
+    public String sender;
+    public String receiver;
+
+
+}
